6dc6f4a1038cdeeb71aa3ef8ded93a2fe04f4a2d
deff <olegkotsubko@gmail.com>
2017-09-13 10:20:35 +0300

    search(100%) Second commit

---
+++ dist/08.technologies-list.php
@@ -0,0 +1,142 @@
+<?php include_once "parts/head.php" ?>
+<body>
+<?php include_once "parts/header.php" ?>
+
+<?php include_once "parts/breadcrumbs.php" ?>
+
+<div class="container">
+    <ul class="technologies-list">
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                   <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                   <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+        <li class="product-technologies__item">
+            <a href="#">
+                <span class="product-technologies__item--image">
+                    <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+                </span>
+                <span class="product-technologies__item--text text-bold">
+                    <span>Данная модель ботинок оснащена надежной защитой для ног - усиленный носок</span>
+                </span>
+            </a>
+        </li>
+    </ul>
+
+    <?php include_once "parts/pagination.php" ?>
+
+</div>
+<br>
+<br>
+
+<?php include_once "parts/footer.php" ?>
+
+<script src="js/libs.min.js"></script>
+<script src="js/common.js"></script>
+</body>
+</html>
---
+++ dist/09.technology-detail.php
@@ -0,0 +1,203 @@
+<?php include_once "parts/head.php" ?>
+<body>
+<?php include_once "parts/header.php" ?>
+
+<?php include_once "parts/breadcrumbs.php" ?>
+
+<div class="container">
+    <div class="technology-detail">
+        <div class="technology-detail__image">
+            <div class="technology-detail__image--wrapper">
+                <img src="img/jpeg_files/tech-1.jpg" alt="tech" width="303" height="200">
+            </div>
+        </div>
+        <div class="technology-detail__text">
+            <p>
+                Создание бренда М-Тас (Militarist Тactical) - новый шаг в развитии ТМ «Милитарист». Имея десятилетний
+                опыт работы с полевым и тактическим снаряжением, компания объединила усилия украинских и зарубежных
+                производителей для создания качественного снаряжения, полевой формы, обуви и аксессуаров. Одним из
+                главных приоритетов при создании продукции бренда М-Тас является поиск оптимального сочетания цены и
+                качества товара.
+
+                Создание бренда М-Тас (Militarist Тactical) - новый шаг в развитии ТМ «Милитарист». Имея десятилетний
+                опыт работы с полевым и тактическим снаряжением, компания объединила усилия украинских и зарубежных
+                производителей для создания качественного снаряжения, полевой формы, обуви и аксессуаров. Одним из
+                главных приоритетов при создании продукции бренда М-Тас является поиск оптимального сочетания цены и
+                качества товара.
+            </p>
+        </div>
+    </div>
+
+    <section class="product-section product-slider-section">
+        <h4 class="product-property-title uk-text-uppercase">Товары с этой технологией</h4>
+        <div class="product-fourItem-slider">
+            <div>
+                <div class="goods">
+                    <div class="goods-top-block">
+                        <div class="goods-image">
+                            <a href="#">
+                                <img src="img/jpeg_files/goods-1.jpg" alt="goods" width="263" height="263">
+                            </a>
+                        </div>
+                        <div class="goods-link">
+                            <a href="#">БОТИНКИ ПОЛЕВЫЕ MK.4 MULTICAM</a>
+                        </div>
+                    </div>
+                    <div class="goods-bottom-block">
+                        <div class="goods-price">
+                            <span class="goods-price__old">
+                                <span>1500</span>
+                                <span>грн</span>
+                            </span>
+                            <span class="goods-price__current">
+                                <span>945</span>
+                                <span>грн</span>
+                            </span>
+                        </div>
+                        <div class="goods-read-more">
+                            <a href="#" class="btn btn-normal btn-black btn-hover-normal">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="goods">
+                    <div class="goods-top-block">
+                        <div class="goods-image">
+                            <a href="#">
+                                <img src="img/jpeg_files/goods-2.jpg" alt="goods" width="263" height="263">
+                            </a>
+                        </div>
+                        <div class="goods-link">
+                            <a href="#">МИЛТЕК ЖИЛЕТ РАЗГРУЗОЧНЫЙ SAAV M83 ВУДЛЕНД</a>
+                        </div>
+                    </div>
+                    <div class="goods-bottom-block">
+                        <div class="goods-price">
+                            <span class="goods-price__current">
+                                <span>450</span>
+                                <span>грн</span>
+                            </span>
+                        </div>
+                        <div class="goods-read-more">
+                            <a href="#" class="btn btn-normal btn-black btn-hover-normal">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="goods">
+                    <div class="goods-top-block">
+                        <div class="goods-image">
+                            <a href="#">
+                                <img src="img/jpeg_files/goods-3.jpg" alt="goods" width="263" height="263">
+                            </a>
+                        </div>
+                        <div class="goods-link">
+                            <a href="#">ШТАНЫ ЗИМНИЕ АRMY PANTS COYOTE</a>
+                        </div>
+                    </div>
+                    <div class="goods-bottom-block">
+                        <div class="goods-price">
+                            <span class="goods-price__current">
+                                <span>1475</span>
+                                <span>грн</span>
+                            </span>
+                        </div>
+                        <div class="goods-read-more">
+                            <a href="#" class="btn btn-normal btn-black btn-hover-normal">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="goods">
+                    <div class="goods-top-block">
+                        <div class="goods-image">
+                            <a href="#">
+                                <img src="img/jpeg_files/goods-3.jpg" alt="goods" width="263" height="263">
+                            </a>
+                        </div>
+                        <div class="goods-link">
+                            <a href="#">ШАРФ-ТРУБА SKULL КОЙОТ</a>
+                        </div>
+                    </div>
+                    <div class="goods-bottom-block">
+                        <div class="goods-price">
+                            <span class="goods-price__current">
+                                <span>120</span>
+                                <span>грн</span>
+                            </span>
+                        </div>
+                        <div class="goods-read-more">
+                            <a href="#" class="btn btn-normal btn-black btn-hover-normal">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="goods">
+                    <div class="goods-top-block">
+                        <div class="goods-image">
+                            <a href="#">
+                                <img src="img/jpeg_files/goods-1.jpg" alt="goods" width="263" height="263">
+                            </a>
+                        </div>
+                        <div class="goods-link">
+                            <a href="#">БОТИНКИ ПОЛЕВЫЕ MK.4 MULTICAM</a>
+                        </div>
+                    </div>
+                    <div class="goods-bottom-block">
+                        <div class="goods-price">
+                            <span class="goods-price__old">
+                                <span>1500</span>
+                                <span>грн</span>
+                            </span>
+                            <span class="goods-price__current">
+                                <span>945</span>
+                                <span>грн</span>
+                            </span>
+                        </div>
+                        <div class="goods-read-more">
+                            <a href="#" class="btn btn-normal btn-black btn-hover-normal">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="goods">
+                    <div class="goods-top-block">
+                        <div class="goods-image">
+                            <a href="#">
+                                <img src="img/jpeg_files/goods-2.jpg" alt="goods" width="263" height="263">
+                            </a>
+                        </div>
+                        <div class="goods-link">
+                            <a href="#">МИЛТЕК ЖИЛЕТ РАЗГРУЗОЧНЫЙ SAAV M83 ВУДЛЕНД</a>
+                        </div>
+                    </div>
+                    <div class="goods-bottom-block">
+                        <div class="goods-price">
+                            <span class="goods-price__current">
+                                <span>450</span>
+                                <span>грн</span>
+                            </span>
+                        </div>
+                        <div class="goods-read-more">
+                            <a href="#" class="btn btn-normal btn-black btn-hover-normal">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <br><br>
+</div>
+
+<?php include_once "parts/footer.php" ?>
+
+<script src="js/libs.min.js"></script>
+<script src="js/common.js"></script>
+</body>
+</html>
